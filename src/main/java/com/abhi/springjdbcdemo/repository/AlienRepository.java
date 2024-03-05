314872206e24167aeb36b9288514de282edc8bf7
Abhishek Kumar <ak35540@gmail.com>
2024-03-06 04:35:38 +0530

    Push

--- src/main/java/com/abhi/springjdbcdemo/repository/AlienRepository.java
+++ src/main/java/com/abhi/springjdbcdemo/repository/AlienRepository.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
